8fe440244a24ee5576c82d02b42ea69caca1132f
aman <amanraj1172004@gmail.com>
2024-10-01 03:12:11 +0530

    lo

--- back/controller/authController.js
+++ back/controller/authController.js
@@ -45,9 +45,31 @@ try {
 }
 
 }
-export const login = (req, res)=>{
-    
+export const login = async (req, res,next)=>{
+    try {
+        const{email, password} = req.body
+        const validUser = await User.findOne({email})
+        if(!validUser){
+           return next(errorHandler(404,"user not found"))
+        }
+        const validPassword = bcrypt.compare(password,hashedPassword)
+        if(!validPassword){
+            return next(errorHandler(401,"wrong credentials"))
+        }
+        const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET)
+
+        res.cookie("access_token",token,{httpOnly:true}).status(200).json({
+            _id:validUser._id,
+            username: validUser.username,
+            email:validUser.email,
+            profilePic:validUser.profilePic,
+            token:token,
+        })
+
+    } catch (error) {
+        
+    }
 }
-export const logout = (req, res)=>{
+export const logout = (req, res,next)=>{
     
 }
