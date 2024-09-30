fcc8a2ca50aad4e49867f6a5312196045e599ea8
aman <amanraj1172004@gmail.com>
2024-09-30 23:07:21 +0530

    connect

--- back/app.js
+++ back/app.js
@@ -1,7 +1,16 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 dotenv.config()
 const app = express()
+
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log("connected to db"); 
+}).catch((err)=>{
+    console.log(err);
+    
+})
+
 
 
 const PORT = process.env.PORT || 3000
