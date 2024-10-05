a51497f6919e5ed4b8fdeec3fc73b4bce0ba2441
aman <amanraj1172004@gmail.com>
2024-10-05 10:53:29 +0530

    search

--- front/src/components/sidebar/SearchInput.jsx
+++ front/src/components/sidebar/SearchInput.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {BiSearch} from 'react-icons/bi'
+import useConversation from '../../zustand/useConversation'
+import { useGetConversation } from '../../hooks/useGetConversation'
+import { toast } from 'react-toastify'
 const SearchInput = () => {
+  const [search, setSearch] = useState("")
+  const{setSelectedConversation}= useConversation()
+  const{conversation} =useGetConversation()
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (!search) {
+      return
+    }
+    
+    const conversations = conversation.find((conversations) =>
+      conversations.username.toLowerCase().includes(search.toLowerCase())
+    )
+
+    if (conversation) {
+      setSelectedConversation(conversations)
+      setSearch("")
+    } else {
+      toast.error("No user found with this username")
+    }
+  }
+
+
   return (
-    <form className='flex items-center gap-2'>
-      <input type="text"placeholder='Search...' className='input input-bordered rounded-full' />
+    <form className='flex items-center gap-2'onSubmit={handleSubmit}>
+      <input type="text"placeholder='Search...'
+       className='input input-bordered rounded-full'
+       value={search}
+       onChange={(e)=>setSearch(e.target.value)} />
+
       <button type='submit' className='btn btn-circle '>
         <BiSearch className="w-6 h-6 outline-none"/>
       </button>
